3c1cf6c80dc157e397bd77b8b8e1541935075784
Poorav <pooravrawat@gmail.com>
2025-11-23 18:48:18 -0500

    Setup done

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+struct Process {
+    int pid;
+    int burstTime;
+};
+
+int main() {
+    return 0;
+}
